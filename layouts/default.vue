c13cfcfa51396d2e1d1f1168f26afb811fbd1228
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-03-28 09:08:27 +0100

    basic header

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div>
+		<header>
+			<h1 id="title">Octopus</h1>
+			<img src="/Logo-Kyburz.svg" alt="Kyburz Logo" id="logo" />
+		</header>
+		<NuxtPage />
+	</div>
+</template>
+
+<style scoped>
+header {
+	background: #c4c2c2;
+	padding: 0.1rem 2rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: 75px;
+}
+
+#title {
+	font-size: 50px;
+	color: rgb(23, 65, 152);
+}
+
+#logo {
+	height: 50%;
+	max-width: 25%;
+	min-width: 150px;
+}
+</style>
