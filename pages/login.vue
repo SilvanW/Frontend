79b03ac18a27d5f1d0d3765a3d611f1b5b8c65ff
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-03-28 11:33:44 +0100

    login skeleton

---
+++ pages/login.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+function say(username, password) {
+    alert(username)
+    alert(password)
+}
+</script>
+
+<template>
+    <div id="test" class="flex flex-col justify-center items-center flex-nowrap">
+        <form class="rounded bg-gray-500">
+            <div class="p-2">
+                <label class="block">Username: {{ username }}</label>
+                <input v-model="username" placeholder="Enter Username"
+                    class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2" />
+            </div>
+            <div class="p-2">
+                <label class="block">Password: {{ password }}</label>
+                <input v-model="password" placeholder="Enter Password" type="password"
+                    class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2" />
+            </div>
+            <div class="p-2">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+                    @click="say(username, password)">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
