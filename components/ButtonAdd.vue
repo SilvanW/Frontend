eb2025b591d937ac8584c05ffe5b7b8dc96071d4
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-04 21:55:16 +0200

    Initial ButtonAdd Component

---
+++ components/ButtonAdd.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps(["label"])
+</script>
+
+<template>
+    <button class="button-add py-2 px-4 rounded w-full ">
+        <div class="flex flex-row justify-center items-center">
+            <Icon name="fa6-solid:plus" color="white" size="1em"></Icon>
+            <h3 class="px-1">{{ label }}</h3>
+        </div>
+    </button>
+</template>
+
+<style scoped>
+.button-add {
+    background-color: var(--orange);
+    color: var(--main_background)
+}
+</style>
