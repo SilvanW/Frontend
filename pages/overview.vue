7309cd13f9bf75da4a2ae6bd843f56490d2f9161
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-04 21:57:31 +0200

    overview page

---
+++ pages/overview.vue
@@ -0,0 +1,23 @@
+<template>
+    <Main>
+        <Card title="Willkommen">
+            <div class="p-2">
+                <TextOutput label="Benutzer" value="Silvan Wiedmer" />
+                <TextOutput label="Funktion" value="Developer" />
+                <TextOutput label="Berechtigung" value="Administrator" />
+                <TextOutput label="IP-Addresse" value="10.242.2.2" />
+                <br>
+                <TextOutput label="Bei Problemen bitte ein Foto des Fehlers machen und an Yunus Ruff melden!"
+                    value="" />
+            </div>
+        </Card>
+        <Card title="Yeet">
+            <div class="p-2">
+                <TextOutput label="Benutzer" value="Silvan Wiedmer" />
+                <TextOutput label="Funktion" value="Developer" />
+                <TextOutput label="Berechtigung" value="Administrator" />
+                <TextOutput label="IP-Addresse" value="10.242.2.2" />
+            </div>
+        </Card>
+    </Main>
+</template>
