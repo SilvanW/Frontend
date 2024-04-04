6369a6bfc940c46b432cde996eb7f088399d3370
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-04 21:56:38 +0200

    Initial TextOutput Component

---
+++ components/TextOutput.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(["label", "value"])
+</script>
+
+<template>
+    <div class="flex flex-row justify-between items-center flex-nowrap">
+        <h2 class="font-bold">{{ label }}</h2>
+        <h2>{{ value }}</h2>
+    </div>
+</template>
