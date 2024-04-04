00fd93d86d1e752ce35836033e392e2f40b645b6
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-04 21:56:49 +0200

    define base colors

--- app.vue
+++ app.vue
@@ -11,6 +11,16 @@ useHead({
 </script>
 
 <style>
+:root {
+  --header_background: rgb(255, 255, 255);
+  --logo_text_color: rgb(23, 65, 152);
+
+  --main_background: rgb(244, 244, 244);
+
+  --red: rgb(255, 27, 28);
+  --orange: rgb(255, 165, 0)
+}
+
 body {
   padding: 0px;
   margin: 0px;
