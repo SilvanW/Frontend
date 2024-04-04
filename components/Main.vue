15ed1738c128ca31b3a77742ac4cdc8650f69272
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-04 21:55:05 +0200

    Initial Main Component

---
+++ components/Main.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="flex-1 flex flex-col justify-center items-center flex-nowrap">
+        <slot />
+    </main>
+</template>
+
+<style scoped>
+main {
+    background-color: var(--main_background);
+}
+</style>
