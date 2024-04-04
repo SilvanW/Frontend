5383dd17870797f600595d23ffa05b2e3356c56c
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-04 21:54:55 +0200

    Initial Card Component

---
+++ components/Card.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps(["title"])
+</script>
+
+<template>
+    <div class="rounded-lg p-2.5 shadow-md card m-2.5">
+        <h2 class="title p-2 font-bold text-2xl">{{ title }}</h2>
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.card {
+    background-color: rgb(225, 225, 225);
+    min-width: 300px;
+}
+
+.title {
+    color: var(--logo_text_color)
+}
+</style>
