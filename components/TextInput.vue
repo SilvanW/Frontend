92fbfbb19cf27334f11b540010186368867fded4
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-05 08:17:22 +0200

    Initial TextInput Component

---
+++ components/TextInput.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(["label", "placeholder"])
+
+const inputValue = ref('')
+</script>
+
+<template>
+    <div class="p-2">
+        <label class="block pb-1 font-medium">{{ label }}</label>
+        <input v-model="inputValue" :placeholder="placeholder"
+            class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2 w-full" />
+    </div>
+</template>
