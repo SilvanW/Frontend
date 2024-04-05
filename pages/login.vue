97f176442b45e4d44f84cd1b081748041cdf0557
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-05 08:17:32 +0200

    use text input component

--- pages/login.vue
+++ pages/login.vue
@@ -14,17 +14,9 @@ function say(username, password) {
 <template>
     <Main>
         <Card title="Login">
-            <form>
-                <div class="p-2">
-                    <label class="block">Username: {{ username }}</label>
-                    <input v-model="username" placeholder="Enter Username"
-                        class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2" />
-                </div>
-                <div class="p-2">
-                    <label class="block">Password: {{ password }}</label>
-                    <input v-model="password" placeholder="Enter Password" type="password"
-                        class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2" />
-                </div>
+            <form class="w-ful">
+                <TextInput label="Username" placeholder="Enter Username" />
+                <TextInput label="Password" placeholder="Enter Password" />
                 <div class="p-2">
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
                         @click="say(username, password)">Login</button>
