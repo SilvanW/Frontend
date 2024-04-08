a502ebcfd694505bb496115579f20283dcfcdb36
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-08 20:22:06 +0200

    Initial InputError Component

---
+++ components/InputError.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(["condition", "text"])
+</script>
+
+<template>
+    <div v-if="condition" class="input-error m-2 p-2 text-center rounded-lg bg-red-100">
+        {{ text }}
+    </div>
+</template>
+
+<style scoped>
+.input-error {
+    color: var(--red)
+}
+</style>
