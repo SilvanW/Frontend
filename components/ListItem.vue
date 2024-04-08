6ee23793b49c1741e6035a8ac358d94e0e463774
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-08 21:30:03 +0200

    Initial list time

---
+++ components/ListItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(["title", "subtitle"])
+</script>
+
+<template>
+    <div class="list p-2 my-2 rounded-lg text-left">
+        <h2 class="font-bold text-lg">
+            {{ title }}
+        </h2>
+        <h3>
+            {{ subtitle }}
+        </h3>
+    </div>
+</template>
+
+<style scoped>
+.list {
+    background-color: var(--main_background)
+}
+</style>
