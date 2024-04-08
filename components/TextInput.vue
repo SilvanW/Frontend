779aca0274b1395175fd18baf72b37b107c76ce2
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-08 20:22:16 +0200

    add Model

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -1,13 +1,14 @@
 <script setup>
-defineProps(["label", "placeholder"])
+defineProps(["label", "placeholder", "type", "required"])
 
-const inputValue = ref('')
+const model = defineModel()
+
 </script>
 
 <template>
     <div class="p-2">
         <label class="block pb-1 font-medium">{{ label }}</label>
-        <input v-model="inputValue" :placeholder="placeholder"
+        <input v-model="model" :placeholder="placeholder" :type="type" :required="required"
             class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2 w-full" />
     </div>
 </template>
