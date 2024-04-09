a8e3780d1634ce54309b31b4a2aef6716babd1bb
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-09 18:53:58 +0200

    add close icon to Card, use when closable is true

--- components/Card.vue
+++ components/Card.vue
@@ -1,10 +1,16 @@
 <script setup>
-defineProps(["title"])
+defineProps(["title", "closable"])
+
+const model = defineModel()
 </script>
 
 <template>
     <div class="rounded-lg p-2.5 shadow-md card ml-2.5 mt-2.5">
-        <h2 class="title p-2 font-medium text-2xl">{{ title }}</h2>
+        <div class="flex flex-row justify-between items-center">
+            <h2 class="title p-2 font-medium text-2xl">{{ title }}</h2>
+            <Icon v-show="closable" v-on:click="model = false" class="card-icon cursor-pointer" name="fa6-solid:xmark"
+                size="1.5em" />
+        </div>
         <slot />
     </div>
 </template>
@@ -17,5 +23,9 @@ defineProps(["title"])
 
 .title {
     color: var(--logo_text_color)
+}
+
+.card-icon {
+    color: var(--red)
 }
 </style>
