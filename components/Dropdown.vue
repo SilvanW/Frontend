9bb87ff1a9b52a2d59e2d3c6b53016656eceec6c
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-09 18:54:09 +0200

    initial Dropdown component

---
+++ components/Dropdown.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(["label", "options", "required"])
+
+const model = defineModel()
+</script>
+
+<template>
+    <div class="py-2">
+        <label class="block pb-1 font-medium">{{ label }}</label>
+        <select class="bg-white p-2 w-full rounded-lg" v-model="model" :required="required">
+            <option v-for="option in options" :value="option.value" :key="option.value">
+                {{ option.text }}</option>
+        </select>
+    </div>
+</template>
