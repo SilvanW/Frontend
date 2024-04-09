b39a41f6075f371456e91e8d814d5836b10f466a
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-09 18:54:38 +0200

    apply padding on all sides

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -6,7 +6,7 @@ const model = defineModel()
 </script>
 
 <template>
-    <div class="p-2">
+    <div class="py-2">
         <label class="block pb-1 font-medium">{{ label }}</label>
         <input v-model="model" :placeholder="placeholder" :type="type" :required="required"
             class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2 w-full" />
