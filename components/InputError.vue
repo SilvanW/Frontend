ccf8c5cf393fadb89d9115760029336934c8ca0d
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-12 13:40:01 +0200

    remove margin

--- components/InputError.vue
+++ components/InputError.vue
@@ -3,7 +3,7 @@ defineProps(["condition", "text"])
 </script>
 
 <template>
-    <div v-if="condition" class="input-error m-2 p-2 text-center rounded-lg bg-red-100">
+    <div v-if="condition" class="input-error p-2 text-center rounded-lg bg-red-100">
         {{ text }}
     </div>
 </template>
