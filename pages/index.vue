176bd3076c51f4696c6d644ef1cacb459c160303
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-16 16:25:43 +0200

    remove unused information

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,6 @@ const showPopup = ref(false)
             <div class="p-2">
                 <TextOutput label="Benutzer" value="Silvan Wiedmer" />
                 <TextOutput label="Funktion" value="Developer" />
-                <TextOutput label="Berechtigung" value="Administrator" />
-                <TextOutput label="IP-Addresse" value="10.242.2.2" />
-                <br>
-                <TextOutput label="Bei Problemen bitte ein Foto des Fehlers machen und an Yunus Ruff melden!"
-                    value="" />
             </div>
         </Card>
     </Main>
