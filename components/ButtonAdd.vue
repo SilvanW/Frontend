da352f143a9a28dc23b124770b4cc6c948bb1514
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-23 08:39:43 +0200

    replace with bootstrap button

--- components/ButtonAdd.vue
+++ components/ButtonAdd.vue
@@ -3,17 +3,10 @@ defineProps(["label"])
 </script>
 
 <template>
-    <button class="button-add py-2 px-4 rounded-lg w-full ">
+    <b-button button="primary" class="button-add py-2 px-4 rounded-lg w-full ">
         <div class="flex flex-row justify-center items-center">
             <Icon name="fa6-solid:plus" color="white" size="1em"></Icon>
             <h3 class="px-1">{{ label }}</h3>
         </div>
-    </button>
+    </b-button>
 </template>
-
-<style scoped>
-.button-add {
-    background-color: var(--orange);
-    color: var(--main_background)
-}
-</style>
