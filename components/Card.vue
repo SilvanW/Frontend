cf7538bcbabc34e8403dd0551101fbfa0bf40ca7
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-23 16:51:45 +0200

    fix border styling from bootstrap

--- components/Card.vue
+++ components/Card.vue
@@ -5,7 +5,7 @@ const model = defineModel()
 </script>
 
 <template>
-    <div class="rounded-lg p-2.5 shadow-md card ml-2.5 mt-2.5">
+    <div class="rounded-lg p-2.5 shadow-md card ml-2.5 mt-2.5 border-0">
         <div class="flex flex-row justify-between items-center">
             <h2 class="title p-2 font-medium text-2xl">{{ title }}</h2>
             <Icon v-show="closable" v-on:click="model = false" class="card-icon cursor-pointer" name="fa6-solid:xmark"
