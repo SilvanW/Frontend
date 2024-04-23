dd8bc1adf24777691c572bfd4941570de64847e4
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-23 08:32:17 +0200

    add bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/supabase']
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/supabase', 'usebootstrap'],
+  usebootstrap: {
+    bootstrap: {
+      prefix: ``
+    },
+    html: {
+      prefix: `B`
+    },
+  },
+  css: [
+    "bootstrap/scss/bootstrap.scss"
+  ]
 })
