c9c8cf7bd55625a069b3a2e9c4c484261f1b6400
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-23 16:51:33 +0200

    add tooltips to buttons

--- components/ButtonChange.vue
+++ components/ButtonChange.vue
@@ -1,12 +1,13 @@
 <script setup>
-defineProps(["label"])
+defineProps(["label", "tooltip"])
 </script>
 
 <template>
-    <b-button button="primary" class="py-2 px-4 rounded-lg w-full ">
+    <b-button button="primary" class="my-2 py-2 px-4 rounded-lg w-full" toggle="tooltip" placement="bottom"
+        :title="tooltip">
         <div class="flex flex-row justify-center items-center">
             <Icon name="fa6-solid:pencil" color="white" size="1em"></Icon>
-            <h4 class="px-1 m-1">{{ label }}</h4>
+            <h5 class="px-1 m-1">{{ label }}</h5>
         </div>
     </b-button>
 </template>
--- components/ButtonDelete.vue
+++ components/ButtonDelete.vue
@@ -1,12 +1,13 @@
 <script setup>
-defineProps(["label"])
+defineProps(["label", "tooltip"])
 </script>
 
 <template>
-    <b-button button="danger" class="my-1 w-full ">
+    <b-button button="danger" class="my-2 py-2 px-4 rounded-lg w-full" toggle="tooltip" placement="bottom"
+        :title="tooltip">
         <div class="flex flex-row justify-center items-center">
             <Icon name="fa6-solid:trash" color="white" size="1em"></Icon>
-            <h4 class="px-1 m-1">{{ label }}</h4>
+            <h5 class="px-1 m-1">{{ label }}</h5>
         </div>
     </b-button>
 </template>
