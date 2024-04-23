d91edd997cc58121e1649c05b02c980ef8af91ba
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-23 09:14:37 +0200

    fix font size

--- components/TextOutput.vue
+++ components/TextOutput.vue
@@ -4,7 +4,7 @@ defineProps(["label", "value"])
 
 <template>
     <div class="flex flex-row justify-between items-center flex-nowrap">
-        <h2 class="font-bold">{{ label }}</h2>
-        <h2>{{ value }}</h2>
+        <h5 class="font-bold">{{ label }}</h5>
+        <h5>{{ value }}</h5>
     </div>
 </template>
