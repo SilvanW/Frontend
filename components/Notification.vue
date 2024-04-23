f953d95a7a0a9830bb789fb18e365e6fb52f62bb
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-23 16:00:58 +0200

    fix spacing on notification

--- components/Notification.vue
+++ components/Notification.vue
@@ -8,7 +8,7 @@ defineProps(["text"])
 <template>
     <div class="popup-container flex flex-col justify-top items-end fixed top-20 right-0 z-1">
         <div class="popup rounded-lg p-2 m-2 border-green-500 border-2">
-            <h2 class="p-2 font-medium text-green-500">{{ text }}</h2>
+            <h2 class="p-2 m-0 font-medium text-green-500">{{ text }}</h2>
         </div>
     </div>
 </template>
