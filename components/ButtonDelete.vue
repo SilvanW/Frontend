233496ca8b6ffc81cf2c8972b347e13b43217c26
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-04-23 20:38:37 +0200

    remove rounding from buttons

--- components/ButtonDelete.vue
+++ components/ButtonDelete.vue
@@ -3,8 +3,7 @@ defineProps(["label", "tooltip"])
 </script>
 
 <template>
-    <b-button button="danger" class="my-2 py-2 px-4 rounded-lg w-full" toggle="tooltip" placement="bottom"
-        :title="tooltip">
+    <b-button button="danger" class="my-2 py-2 px-4 w-full" toggle="tooltip" placement="bottom" :title="tooltip">
         <div class="flex flex-row justify-center items-center">
             <Icon name="fa6-solid:trash" color="white" size="1em"></Icon>
             <h5 class="px-1 m-1">{{ label }}</h5>
