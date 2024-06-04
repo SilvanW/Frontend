e4ce8aefecb3e140472fea9724a726f4a29382ca
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-04 15:30:54 +0200

    add pinia for storages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/supabase', 'nuxt-file-storage'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/supabase', 'nuxt-file-storage', '@pinia/nuxt'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
-  }
+  },
+  ssr: false
 })
