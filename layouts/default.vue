9cfdcee4b848a2045bc86a9505fec5a9d0a1ba18
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-04 15:31:57 +0200

    remove loading indicator

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,7 +17,6 @@ async function logout() {
 </script>
 
 <template>
-	<NuxtLoadingIndicator />
 	<div class="h-screen flex flex-col">
 		<header>
 			<div class="flex flex-row justify-center items-center">
