3646844696e9648233f4b59ca1b5317a01ac5226
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-04 18:24:20 +0200

    use id and name due to database design

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -8,8 +8,8 @@ const model = defineModel()
     <div class="py-2">
         <label class="block pb-1 font-medium">{{ label }}</label>
         <select class="bg-white p-2 w-full rounded-lg" v-model="model" :required="required">
-            <option v-for="option in options" :value="option.value" :key="option.value">
-                {{ option.text }}</option>
+            <option v-for="option in options" :value="option.id" :key="option.id">
+                {{ option.name }}</option>
         </select>
     </div>
 </template>
