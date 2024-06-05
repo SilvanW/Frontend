44a3abb0be1837af8103c92b674f5cf59216e5ee
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-05 16:48:51 +0200

    use different border radius

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -4,7 +4,7 @@ defineProps(["title", "subtitle", "image"])
 
 <template>
     <div class="list p-2 my-2 rounded-lg text-left cursor-pointer flex flex-row gap-2">
-        <img v-if="image" :src="image" class="aspect-square object-cover rounded-sm" height="50px" width="50px" />
+        <img v-if="image" :src="image" class="aspect-square object-cover rounded-md" height="50px" width="50px" />
         <div>
             <h5 class="font-bold">
                 {{ title }}
