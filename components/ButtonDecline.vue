2aac4457f35db45a7dcef31c0ccd221f2237a46b
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-05 20:57:02 +0200

    add decline button to components

---
+++ components/ButtonDecline.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps(["label", "tooltip"])
+</script>
+
+<template>
+    <Tooltip :tooltip="tooltip">
+        <button class="btn btn-error my-2 py-2 px-4 w-full">
+            <div class="flex flex-row justify-center items-center">
+                <Icon name="fa6-solid:xmark" color="white" size="1em"></Icon>
+                <h5 class="px-1 m-1 text-white">{{ label }}</h5>
+            </div>
+        </button>
+    </Tooltip>
+</template>
