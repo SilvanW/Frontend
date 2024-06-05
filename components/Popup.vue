953400e1073585a99563c16b49e5e6296d9d5e20
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-05 20:59:18 +0200

    add popup to settings change

--- components/Popup.vue
+++ components/Popup.vue
@@ -6,14 +6,14 @@ const model = defineModel()
 
 <template>
     <div class="popup-container flex flex-col justify-center items-center fixed top-20 left-0 right-0 bottom-0 z-1">
-        <div class="popup rounded-lg m-2">
+        <div class="popup rounded-lg">
             <div class="flex flex-row justify-between items-center p-2">
                 <h2 class="title p-2 font-medium text-2xl">{{ title }}</h2>
                 <Icon v-on:click="model = false" class="popup-icon cursor-pointer" name="fa6-solid:xmark"
                     size="1.5em" />
             </div>
 
-            <div class="p-4">
+            <div class="px-4 pb-2">
                 <slot />
             </div>
         </div>
--- pages/settings.vue
+++ pages/settings.vue
@@ -11,7 +11,11 @@ const currentUserStore = useCurrentUser()
 
 const selectedLanguage = ref('de')
 
+// Popup state
+const showChangeSettingsPopup = ref(false)
+
 async function changeSettings() {
+    showChangeSettingsPopup.value = false
     setLocale(selectedLanguage.value)
     currentUserStore.updateDefaultLanguage(selectedLanguage.value)
 }
@@ -30,8 +34,15 @@ onMounted(async () => {
         <Card :title="$t('settings')" :skeleton="true">
             <div class="p-2">
                 <Dropdown v-model="selectedLanguage" :label="$t('language')" :options="availableLanguages" />
-                <ButtonChange @click="changeSettings()" :label="$t('changeSettings')" />
+                <ButtonChange @click="showChangeSettingsPopup = true" :label="$t('changeSettings')" />
             </div>
         </Card>
     </Main>
+    <Popup v-if="showChangeSettingsPopup" title="Änderungen übernehmen" v-model="showChangeSettingsPopup">
+        <h1>Sollen die Änderungen wirklich übernommen werden?</h1>
+        <div class="flex gap-2">
+            <ButtonDecline @click="showChangeSettingsPopup = false" label="Nein" />
+            <ButtonAccept @click="changeSettings" label="Ja" />
+        </div>
+    </Popup>
 </template>
