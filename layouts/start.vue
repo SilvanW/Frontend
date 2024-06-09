fcb6fca002b9cd7736db81d27f9f658d88199673
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-09 13:01:31 +0200

    add logo responsiveness to start layout

--- layouts/start.vue
+++ layouts/start.vue
@@ -29,5 +29,16 @@ header {
     height: 50%;
     max-width: 25%;
     min-width: 150px;
+    display: none;
+    /*dont show logo when screen to small, as it does not provide any funcionality*/
+}
+
+@media (min-width: 600px) {
+    #logo {
+        height: 50%;
+        max-width: 25%;
+        min-width: 150px;
+        display: inline;
+    }
 }
 </style>
