7f20f23c9c07017535d6cd5a5dfd7015271729a5
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-12 21:20:39 +0200

    add gap for better readability

--- components/TextOutput.vue
+++ components/TextOutput.vue
@@ -3,7 +3,7 @@ defineProps(["label", "value"])
 </script>
 
 <template>
-    <div class="flex flex-row justify-between items-center flex-nowrap">
+    <div class="flex flex-row justify-between items-center flex-nowrap gap-2.5">
         <h5 class="font-bold">{{ label }}</h5>
         <h5>{{ value }}</h5>
     </div>
