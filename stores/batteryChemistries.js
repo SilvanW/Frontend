8a34638fce2f732b8ebd04553082ae04f8964f41
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-12 20:53:51 +0200

    add alert for unexpected errors

--- stores/batteryChemistries.js
+++ stores/batteryChemistries.js
@@ -7,7 +7,7 @@ export const useBatteryChemistries = defineStore('batteryChemistries', () => {
         const { data: battery_chemistries_data, error: battery_chemistries_error } = await supabase.from("battery_chemistries").select()
 
         if (battery_chemistries_error) {
-            console.log(battery_chemistries_error)
+            so(battery_chemistries_error)
             alert("An unknown error has occurred. Please try again later")
             return
         }
