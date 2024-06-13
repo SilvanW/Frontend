30fe66787ce826e5e3d37c00b311bc6dc4e0f569
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2024-06-13 15:08:59 +0200

    add spacing to input error

--- components/InputError.vue
+++ components/InputError.vue
@@ -3,7 +3,7 @@ defineProps(["condition", "text"])
 </script>
 
 <template>
-    <div v-if="condition" class="input-error p-2 text-center rounded-lg bg-red-100">
+    <div v-if="condition" class="input-error p-2 text-center rounded-lg bg-red-100 my-2">
         {{ text }}
     </div>
 </template>
